9b140ad01df8330c1b9b52fc1ceb5cc37d01ccd9
gaston borges <gbsm00@gmail.com>
2022-08-16 22:11:07 -0300

    first commit

--- js/funciones.js
+++ js/funciones.js
@@ -1,4 +1,8 @@
 
+const titulo = document.getElementById("titulo");
+const parrafo = document.getElementById("parrafoDescriptivo")
+const agregar = document.getElementsByClassName("btn-outline-secondary")
+
 
 function agregarCancion(){
     let nombre = prompt("Ingrese Nombre del tema:")
@@ -9,20 +13,32 @@ function agregarCancion(){
 
 }
 
-agregarCancion();
+
+function agregarCancionRhcp(){
+    let nombre = prompt("Ingrese Nombre del tema:")
+    let acordes = prompt("Ingrese acordes:")
+    let artista = prompt("Ingrese nombre del Artista")
+    cancionesRHCP.push(new Cancion(nombre, acordes, artista))
+
+}
+
+
 
 function buscarCancionRhcp() {
     let can = prompt("Ingresa la cancion a buscar:")
     const resultado = cancionesRHCP.find(Cancion => Cancion.nombre === can)
         if (resultado){
-            console.table(resultado)
+            const li = document.createElement("li")
+            li.innerText = resultado.nombre
+            lista.append(li)
         } else {
             console.warn("La cancion no se encuentra ingresada en la lista")
         }
     }
 
+    const lista = document.getElementById("lista")
 
-function listaCancionesRhcp(){ 
+    function listaCancionesRhcp(){ 
     for (const Cancion of cancionesRHCP){
       console.log(Cancion.nombre)
           }
@@ -34,7 +50,9 @@ function buscarCancion() {
         let can = prompt("Ingresa la cancion a buscar:")
         const resultado = canciones.find(Cancion => Cancion.nombre === can)
             if (resultado){
-                console.table(resultado)
+                const li = document.createElement("li")
+                              li.innerText = resultado.nombre
+                              lista.append(li)
             } else {
                 console.warn("La cancion no se encuentra ingresada en la lista")
             }
@@ -50,3 +68,15 @@ function buscarCancion() {
                     console.warn("El artista no se encuentra ingresado en la lista")
                 }
             }
+
+function mostrarNombresRHCP() {
+            cancionesRHCP.forEach(Cancion => {
+            
+                        const li = document.createElement("li")
+                              li.innerText = Cancion.nombre
+                              lista.append(li)
+                })
+           
+            }
+agregarCancionRhcp();
+mostrarNombresRHCP();
